e71c5e16d9ad6bb38c33151f026540c7c91cb243
asepkh <asep.naxbkz@gmail.com>
2020-11-02 10:20:59 +0700

    Production

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000/api/v1";
-export const ebookFolder = "http://localhost:5000/ebook/";
-export const thumbnailFolder = "http://localhost:5000/thumbnail/";
+export const baseURL = "https://literatures.herokuapp.com/api/v1";
 export const API = axios.create({
-    baseURL,
+  baseURL,
 });
 
 export function setAuthToken(token) {
-    if (token) {
-        API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    } else {
-        delete API.defaults.headers.common["Authorization"];
-    }
-};
+  if (token) {
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.common["Authorization"];
+  }
+}
