6f57febd25c5205f64fb8b2df4cb737c74f86a86
asepkh <asep.naxbkz@gmail.com>
2020-12-01 10:35:18 +0700

    fix cropper production

--- src/Component/Cropper/index.js
+++ src/Component/Cropper/index.js
@@ -1,6 +1,48 @@
 import React, { useState } from "react";
 import Cropper from "react-easy-crop";
-import { getCroppedImg } from "./CropImage";
+
+const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", (error) => reject(error));
+    image.setAttribute("crossOrigin", "anonymous");
+    image.src = url;
+  });
+
+const getCroppedImg = async (imageSrc, crop, resize) => {
+  try {
+    const image = await createImage(imageSrc);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    /* setting canvas width & height allows us to 
+    resize from the original image resolution */
+    canvas.width = resize.width;
+    canvas.height = resize.height;
+
+    ctx.drawImage(
+      image,
+      crop.x,
+      crop.y,
+      crop.width,
+      crop.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+
+    // As a blob
+    return new Promise((resolve) => {
+      canvas.toBlob((blob) => {
+        resolve(blob);
+      }, "image/png");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const ImageCropper = ({ getBlob, inputImg, aspect, shape, size, resize }) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
